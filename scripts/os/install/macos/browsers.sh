c4fc74fc85337a216e8d29ee125356551422ab4a
Evandro Paes <evandro.reis@rarvos.com>
2019-01-07 17:22:36 -0200

    Changed exec chmod for office.sh and docker.sh

--- scripts/os/install/macos/browsers.sh
+++ scripts/os/install/macos/browsers.sh
@@ -36,14 +36,8 @@ brew_install "Opera" "opera" "caskroom/cask" "cask"
 brew_install "Opera Beta" "opera-beta" "caskroom/versions" "cask"
 brew_install "Opera Developer" "opera-developer" "caskroom/versions" "cask"
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# `Safari Technology Preview` requires macOS 10.11.4 or la
-# https://github.com/evandropaes/dotfiles/issues
-
 if is_supported_version "$(get_os_version)" "10.11.4"; then
     printf "\n"
     brew_install "Safari Technology Preview" "safari-technology-preview" "caskroom/versions" "cask"
 fi
 
-brew_install "WebKit" "webkit-nightly" "caskroom/versions" "cask"
